88a52037ccffe444d9d78d616d2fd2198b970cc9
410063005 <410063005@163.com>
2014-06-28 12:07:21 +0800

    add: log 工具方法
    Signed-off-by: 410063005 <410063005@163.com>

--- src/com/tx/example/nlp/Dbg.java
+++ src/com/tx/example/nlp/Dbg.java
@@ -7,4 +7,12 @@ public class Dbg {
 	public static void i(String tag, String msg) {
 		Log.i(tag, msg);
 	}
+
+	public static void i(String tag, String msg, boolean showThreadName) {
+		if (showThreadName) {
+			Log.i(tag, "[" + Thread.currentThread().getName() + "]:" + msg);
+		} else {
+			Log.i(tag, msg);
+		}
+	}
 }
