97ff8f51d8adfb289017a1356a3fc88a37cc99d9
410063005@163.com <410063005@163.com>
2014-10-28 20:39:57 +0800

    fix: incomplete location
    Signed-off-by: 410063005@163.com <410063005@163.com>

--- src/com/tx/example/nlp/util/Utils.java
+++ src/com/tx/example/nlp/util/Utils.java
@@ -1,12 +1,16 @@
 package com.tx.example.nlp.util;
 
+import android.annotation.SuppressLint;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.Build;
+import android.os.SystemClock;
 
 import com.tencent.map.geolocation.TencentLocation;
 
 public class Utils {
 
+	@SuppressLint("NewApi")
 	public static Location from(TencentLocation location) {
 		Location l = new Location(LocationManager.NETWORK_PROVIDER);
 		l.setLatitude(location.getLatitude());
@@ -14,6 +18,13 @@ public class Utils {
 		l.setAltitude(location.getAltitude());
 		l.setAccuracy(location.getAccuracy());
 		l.setTime(location.getTime());
+		if (Build.VERSION.SDK_INT >= 17) {
+			try {
+				l.setElapsedRealtimeNanos(SystemClock.elapsedRealtimeNanos());
+			} catch (NoSuchMethodError e) {
+				// ignore
+			}
+		}
 		return l;
 	}
 }
