8be25f4c0ee897236b5b6d70faf54d5344516a89
410063005@163.com <410063005@163.com>
2014-10-31 16:22:31 +0800

    add: api 19 support
    Signed-off-by: 410063005@163.com <410063005@163.com>

--- src/com/tx/example/nlp/util/LegacyWrapper.java
+++ src/com/tx/example/nlp/util/LegacyWrapper.java
@@ -1,22 +1,50 @@
 package com.tx.example.nlp.util;
 
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.location.LocationManager;
+import android.os.Build;
 import android.provider.Settings;
 import android.util.Log;
 
 public class LegacyWrapper {
 
+	@SuppressLint("InlinedApi")
 	@SuppressWarnings("deprecation")
 	public static boolean isNetworkLocationProviderEnabled(Context context) {
-		return Settings.Secure.isLocationProviderEnabled(
-				context.getContentResolver(), LocationManager.NETWORK_PROVIDER);
+		if (Build.VERSION.SDK_INT >= 19) {
+			int mode = Settings.Secure.getInt(context.getContentResolver(),
+					Settings.Secure.LOCATION_MODE,
+					Settings.Secure.LOCATION_MODE_OFF);
+			return mode == Settings.Secure.LOCATION_MODE_BATTERY_SAVING
+					|| mode == Settings.Secure.LOCATION_MODE_HIGH_ACCURACY;
+		} else {
+			return Settings.Secure.isLocationProviderEnabled(
+					context.getContentResolver(),
+					LocationManager.NETWORK_PROVIDER);
+		}
 	}
 
+	@SuppressLint("InlinedApi")
 	@SuppressWarnings("deprecation")
 	public static void setNetworkLocationProviderEnabled(Context context,
 			boolean enabled) {
+		if (enabled == isNetworkLocationProviderEnabled(context)) {
+			return;
+		}
+		if (Build.VERSION.SDK_INT >= 19) {
+			try {
+				Settings.Secure.putInt(context.getContentResolver(),
+						Settings.Secure.LOCATION_MODE,
+						Settings.Secure.LOCATION_MODE_HIGH_ACCURACY);
+			} catch (Exception e) {
+				Debug.e("LegacyWrapper", "setNetworkLocationProviderEnabled(api 19): "
+						+ Log.getStackTraceString(e));
+			}
+			return;
+		}
+
 		try {
 			Settings.Secure.setLocationProviderEnabled(
 					context.getContentResolver(),
