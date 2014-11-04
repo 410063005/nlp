2957d857f67d2ebe5d11e7e9ac98b773a5439c50
410063005@163.com <410063005@163.com>
2014-11-04 17:10:06 +0800

    add: isScannable add: test case
    Signed-off-by: 410063005@163.com <410063005@163.com>

--- src/com/tx/example/nlp/util/Utils.java
+++ src/com/tx/example/nlp/util/Utils.java
@@ -1,12 +1,20 @@
 package com.tx.example.nlp.util;
 
+import java.util.List;
+
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.location.Location;
 import android.location.LocationManager;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.SystemClock;
 
 import com.tencent.map.geolocation.TencentLocation;
+import com.tencent.map.geolocation.exp.AppContext;
+import com.tencent.map.geolocation.info.TxCellInfo;
+import com.tencent.map.geolocation.util.SosoLocUtils;
 
 public class Utils {
 
@@ -26,5 +34,32 @@ public class Utils {
 			}
 		}
 		return l;
+	}
+
+	@SuppressLint("NewApi")
+	public static boolean isScannable(WifiManager wifiManager) {
+		if (wifiManager == null) {
+			return false;
+		}
+		if (Build.VERSION.SDK_INT >= 18) {
+			return wifiManager.isScanAlwaysAvailable();
+		} else {
+			return wifiManager.isWifiEnabled();
+		}
+	}
+
+	public static String createMappingData(Context context, Location location, List<ScanResult> aps, TxCellInfo cell) {
+		final String imei = AppContext.getInstance(context).getAppStatus().getDeviceId();
+		final String sep = ",";
+		final String itemSep = ";";
+		String gpsInfoJson = SosoLocUtils.getGpsInfoWithJoiner(location, sep);
+		String wifiInfoJson = SosoLocUtils.getWifiInfoWithJoiner(aps, sep, itemSep);
+		String cellInfoJson = SosoLocUtils.getCellInfoWithJoiner(cell, sep, itemSep);
+
+		StringBuilder sb = new StringBuilder();
+		sb.append(imei).append("|").append(gpsInfoJson).append("|")
+				.append(wifiInfoJson).append("|").append(cellInfoJson)
+				.append("\n");
+		return sb.toString();
 	}
 }
